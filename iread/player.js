7894eb10171316e57b9794a76ac99c3223aaa5df
taiyue <taiyue@ms1.hanlin.com.tw>
2020-01-06 14:51:51 +0800

    iread

---
+++ iread/player.js
@@ -0,0 +1,122 @@
+console.log('hello !');
+
+const imgBlock = document.getElementById("imgBlock");
+const audioBlock = document.getElementById("audioBlock");
+
+const playFlag = false;
+// const audio = document.getElementById("testAudio");
+const audio = document.createElement("audio");
+const source = document.createElement("source");
+const audioErrDes = document.createElement("span");
+
+source.setAttribute("src", "./01.mp3")
+source.setAttribute("type", "audio/mp3");
+audio.appendChild(source);
+audioErrDes.innerText = "瀏覽器不支援，播放器無法正常啟用";
+audio.appendChild(audioErrDes);
+
+audio.setAttribute("controls", "");
+audio.load();
+audioBlock.appendChild(audio);
+
+const imgs = [
+    {
+        startTime: 0,
+        src: "./01-1.jpg"
+    }, 
+    {
+        startTime: 23,
+        src: "./01-2.jpg"
+    }, 
+    {
+        startTime: 34,
+        src: "./01-3.jpg"
+    }
+];
+
+// const imgs = [
+//     {
+//         startTime: 0,
+//         src: "./02-1.png"
+//     }, 
+//     {
+//         startTime: 14.3,
+//         src: "./02-2.png"
+//     }
+// ];
+
+let img = document.createElement("img");
+let imgCurrIdx = 0;
+img.src = imgs[imgCurrIdx].src;
+imgBlock.appendChild(img);
+
+let radios = document.getElementsByName("switch");
+radios[0].setAttribute("checked", "");
+
+let intervalId = 0;
+let deprecateIntervalId = 0;
+
+function bindAudio() {
+    audio.addEventListener("play", (event) => {
+        console.log("play()");
+        let currentTime = audio.currentTime;
+        for (let i=0; i<imgs.length; i++) {
+            if (i+1 >= imgs.length) {
+                img.src = imgs[imgs.length-1].src;
+                imgCurrIdx = imgs.length-1;
+                break;
+            }
+            if (currentTime < imgs[i+1].startTime) {
+                img.src = imgs[i].src;
+                imgCurrIdx = i;
+                break;
+            }
+        }
+    });
+
+    audio.addEventListener("playing", (event) => {
+        console.log("playing()");
+
+    });
+
+    audio.addEventListener("timeupdate", (event) => {
+        console.log("timeupdate()");
+        if (imgCurrIdx+1 >= imgs.length)
+            return;
+        if (audio.currentTime >= imgs[imgCurrIdx+1].startTime) {
+            imgCurrIdx++;
+            if (getSwitch() === "auto")
+                img.src = imgs[imgCurrIdx].src;
+            if (getSwitch() === "pause")
+                audio.pause();
+        }
+    })
+}
+
+function bindKey() {
+    document.onkeydown = event => {
+        let keyCode = event.keyCode;
+        if (keyCode === 32) {
+            if (audio.paused === true) {
+                if (audio.canPlayType("audio/mp3") !== '')
+                    audio.play();
+            } else
+                audio.pause();
+        }
+    }
+}
+
+function getSwitch() {
+    for (let i=0; i < radios.length; i++) {
+        if (radios[i].checked)
+            return radios[i].value;
+    }
+}
+
+function main() {
+    bindKey();
+    bindAudio();
+}
+
+main();
+
---
+++ iread/recorder.js
@@ -0,0 +1,103 @@
+// function 
+
+function tryRecorder() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        // 取得麥克風裝置
+        const deviceId = navigator.mediaDevices.enumerateDevices()
+            .then(devices => devices.find(deviceInfo => deviceInfo.kind === 'audioinput'))
+            .then(deviceInfo => console.log('deviceInfo: ', deviceInfo))
+            .catch(e => console.error(e))
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints
+        const contraints = {
+            audio: {
+                deviceId,
+                sampleRate: 4410,
+                sampleSize: 16,
+            }
+        }
+
+        // if (!deviceId) return;
+
+        // 取得麥克風資料
+        navigator.mediaDevices.getUserMedia(contraints)
+            .then(stream => {
+                const options = { mimeType: 'audio/webm' };
+                const recordedChunks = [];
+                const recorder = new MediaRecorder(stream, options);
+
+                recorder.ondataavailable = e => {
+                    console.log("ondataavailable()");
+                    if (e.data.size > 0) recordedChunks.push(e.data);
+                    // 針對每個 chunk 的處理 ...
+                    let record = recordedChunks[recordedChunks.length - 1];
+                    console.log('record: ', record);
+                    if (recordedChunks.length > 2) {
+                        if (recorder.state === "recording") {
+                            recorder.stop();
+                        }
+                    }
+                };
+
+                recorder.onstop = () => {
+                    // 針對錄製檔案的處理 ...
+                    console.log("onstop()");
+                };
+
+                recorder.start(3000);
+            });
+    }
+}
+
+let recorder = undefined;
+
+function startRecording() {
+    HZRecorder.get(function (rec) {
+        recorder = rec;
+        recorder.start();
+    }, {
+        sampleBits: 16,
+        sampleRate: 16000
+    });
+}
+
+function stopRecording() {
+    recorder.stop();
+    var blob = recorder.getBlob();
+    var url = URL.createObjectURL(blob);
+    var div = document.createElement('div');
+    var au = document.createElement('audio');
+    var hf = document.createElement('a');
+    au.controls = true;
+    au.src = url;
+    hf.href = url;
+    hf.download = new Date().toISOString() + '.wav';
+    hf.innerHTML = hf.download;
+    div.appendChild(au);
+    div.appendChild(hf);
+    recordingslist.appendChild(div);
+}
+function playRecording() {
+    recorder.play(audio);
+}
+function uploadAudio() {
+    recorder.upload("Handler1.ashx", function (state, e) {
+        switch (state) {
+            case 'uploading':
+                //var percentComplete = Math.round(e.loaded * 100 / e.total)   '%';
+                break;
+            case 'ok':
+                //alert(e.target.responseText);
+                alert("上傳成功");
+                break;
+            case 'error':
+                alert("上傳失敗");
+                break;
+            case 'cancel':
+                alert("上傳被取消");
+                break;
+        }
+    });
+}
+
+// startRecording();
