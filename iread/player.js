50d4079c13c2c5f16c02e1bbac61c574bf5ab080
taiyue <taiyue@ms1.hanlin.com.tw>
2020-01-06 17:13:35 +0800

    change resources

--- iread/player.js
+++ iread/player.js
@@ -9,7 +9,7 @@ const audio = document.createElement("audio");
 const source = document.createElement("source");
 const audioErrDes = document.createElement("span");
 
-source.setAttribute("src", "./02.mp3")
+source.setAttribute("src", "./01.mp3")
 source.setAttribute("type", "audio/mp3");
 audio.appendChild(source);
 audioErrDes.innerText = "瀏覽器不支援，播放器無法正常啟用";
@@ -19,31 +19,31 @@ audio.setAttribute("controls", "");
 audio.load();
 audioBlock.appendChild(audio);
 
-// const imgs = [
-//     {
-//         startTime: 0,
-//         src: "./01-1.jpg"
-//     }, 
-//     {
-//         startTime: 23,
-//         src: "./01-2.jpg"
-//     }, 
-//     {
-//         startTime: 34,
-//         src: "./01-3.jpg"
-//     }
-// ];
-
 const imgs = [
     {
         startTime: 0,
-        src: "./02-1.png"
+        src: "./01-1.jpg"
     }, 
     {
-        startTime: 14.3,
-        src: "./02-2.png"
+        startTime: 23,
+        src: "./01-2.jpg"
+    }, 
+    {
+        startTime: 34,
+        src: "./01-3.jpg"
     }
 ];
+
+// const imgs = [
+//     {
+//         startTime: 0,
+//         src: "./02-1.png"
+//     }, 
+//     {
+//         startTime: 14.3,
+//         src: "./02-2.png"
+//     }
+// ];
 
 let img = document.createElement("img");
 let imgCurrIdx = 0;
